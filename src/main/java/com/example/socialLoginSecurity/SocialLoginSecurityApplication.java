11aeddf35e4d80aac244a08101d075c2bce97c77
junseong <junseong@unitsoft.kr>
2022-01-14 10:34:05 +0900

    socialLogin with spring-security

---
+++ src/main/java/com/example/socialLoginSecurity/SocialLoginSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.socialLoginSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialLoginSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialLoginSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/socialLoginSecurity/SocialLoginSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.socialLoginSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialLoginSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
